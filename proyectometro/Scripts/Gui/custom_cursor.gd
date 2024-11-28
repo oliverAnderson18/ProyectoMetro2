9bb7e5206563f73b84b2f79c2499ab145820b6eb
wenjie198 <wenjie1987711563@gmail.com>
2024-11-28 16:37:18 +0100

    Drag cursor

--- proyectometro/Scripts/Gui/custom_cursor.gd
+++ proyectometro/Scripts/Gui/custom_cursor.gd
@@ -5,12 +5,13 @@ var _point = load("res://Assets/Sprites/Mouse/hand_point.png")
 var _grab = load("res://Assets/Sprites/Mouse/hand_closed.png")
 var _disabled = load("res://Assets/Sprites/Mouse/disabled.png")
 var _bracket = load("res://Assets/Sprites/Mouse/bracket_a_vertical.png")
+var _cross = load("res://Assets/Sprites/resize_a_cross.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(_point, Input.CURSOR_POINTING_HAND)
 	Input.set_custom_mouse_cursor(_disabled, Input.CURSOR_FORBIDDEN)
 	Input.set_custom_mouse_cursor(_bracket, Input.CURSOR_IBEAM)
-	
+	Input.set_custom_mouse_cursor(_cross, Input.CURSOR_CROSS)
 
 func _input(event) -> void:
 	if event is InputEventMouseButton:
